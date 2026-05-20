211158d7f08871d6c59ff73ea83d25f145143f76
muhammadfajarr30 <muhammad.fajarr362@gmail.com>
2026-05-20 15:22:02 +0700

    feat: add substraction

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const { multiply, divide } = require("./src/utils/calc.js");
 
 const resultMultiply = multiply(10,5);
 const resultDivide = divide(10,2);
+const resultSum = sum(10,20);
+const resultSubstract = substract(10,5);
 
 console.log(resultMultiply);
 console.log(resultDivide);
+console.log(resultSum);
+console.log(resultSubstract);
--- src/utils/calc.js
+++ src/utils/calc.js
@@ -9,3 +9,7 @@ exports.divide = (num1, num2)=>{
 exports.sum = (num1, num2)=>{
   return num1+num2;
 };
+
+exports.substract = (num1, num2)=>{
+  return num1-num2;
+}
