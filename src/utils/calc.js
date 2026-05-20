ba3c95041fcaa881f1ee9b2527dfce6b43be1fd8
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-20 17:55:12 +0700

    feat: Added validation feature for function parameter values ​​in sum, divide, and multiply functions must be numbers and added check for the second parameter value not being equal to 0 in the divide function.

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -1,23 +1,39 @@
-exports.multiply = (num1,num2)=>{
-  return num1*num2;
-};
-
-exports.divide = (num1, num2)=>{
-  return num1/num2;
-};
-
-exports.sum = (num1, num2)=>{
-  if(num1 && num2 && typeof num1 !== "number" && typeof num2 !== "number"){
+exports.multiply = (num1, num2) => {
+  if (typeof num1 !== "number" && typeof num2 !== "number") {
     const err = new Error("parameter must be a number")
     throw err
   }
-  return num1+num2;
+  return num1 * num2;
 };
 
-exports.substract = (num1, num2)=>{
-  return num1-num2;
+exports.divide = (num1, num2) => {
+  if (typeof num1 !== "number" && typeof num2 !== "number") {
+    const err = new Error("parameter must be a number")
+    throw err
+  }
+  return num1 / num2;
+};
+
+exports.sum = (num1, num2) => {
+  if (num1 && num2 && typeof num1 !== "number" && typeof num2 !== "number") {
+    const err = new Error("parameter must be a number")
+    throw err
+  }
+  return num1 + num2;
+};
+
+exports.substract = (num1, num2) => {
+  if (typeof num1 !== "number" && typeof num2 !== "number") {
+    const err = new Error("parameter must be a number")
+    throw err
+  }
+  return num1 - num2;
 }
 
-exports.modulo = (num1, num2)=>{
-  return num1%num2;
+exports.modulo = (num1, num2) => {
+  if (typeof num1 !== "number" && typeof num2 !== "number") {
+    const err = new Error("parameter must be a number")
+    throw err
+  }
+  return num1 % num2;
 };
