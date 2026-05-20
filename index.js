ffd086d75cfe5d5faac7bf18a730da92bc52fa7a
dimastadeoo <dimastadeoo@gmail.com>
2026-05-20 15:07:21 +0700

    feat: add modulo function for calc.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const { multiply, divide } = require("./src/utils/calc.js");
+const { multiply, divide, sum, modulo } = require("./src/utils/calc.js");
+
 
 const resultMultiply = multiply(10,5);
 const resultDivide = divide(10,2);
 
+
 console.log(resultMultiply);
 console.log(resultDivide);
+console.log(sum(5,6));
+
+console.log(modulo(6,9));
