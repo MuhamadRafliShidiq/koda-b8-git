f233108bf88006f6eb91eb9f57895cb069caba21
fulsep <57238803+fulsep@users.noreply.github.com>
2026-05-20 09:43:44 +0700

    feat: calculation for multiply and divide

---
+++ index.js
@@ -0,0 +1,7 @@
+import { multiply, divide } from "./src/utils/calc.js";
+
+const resultMultiply = multiply(10,5)
+const resultDivide = divide(10,2)
+
+console.log(resultMultiply)
+console.log(resultDivide)
