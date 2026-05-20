31b2453449f2dc101f78937ec49f97dc4ed9ca08
fulsep <57238803+fulsep@users.noreply.github.com>
2026-05-20 16:54:35 +0700

    feat: add validation at sum function in calc.js

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -7,5 +7,9 @@ exports.divide = (num1, num2)=>{
 };
 
 exports.sum = (num1, num2)=>{
+  if(!num1 && !num2 && typeof num1 !== "number" && typeof num2 !== "number"){
+    const err = new Error("parameter must be a number")
+    throw err
+  }
   return num1+num2;
 };
